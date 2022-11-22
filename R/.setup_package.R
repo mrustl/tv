39b5d4b9c5e9f78befad26e6b98fc79ab3b7b994
mrustl <michael@rustler.me>
2022-11-22 21:09:43 +0100

    Create pkg skeleton with kwb.pkgbuild

---
+++ R/.setup_package.R
@@ -0,0 +1,31 @@
+# Enable repository from kwb-r
+options(repos = c(
+  kwbr = 'https://kwb-r.r-universe.dev',
+  CRAN = 'https://cloud.r-project.org'))
+# Download and install kwb.pkgbuild in R
+install.packages('kwb.pkgbuild')
+
+
+pkg <- list(
+  name = "tvl",
+  title = "R Package for TV-L",
+  desc = paste(
+    "R package for TV-L (Tarifvertrag für den Öffentlichen Dienst der Länder)."
+  )
+)
+
+kwb.pkgbuild::use_pkg_skeleton("tvl")
+
+kwb.pkgbuild::use_pkg(
+  pkg = pkg,
+  copyright_holder = list(name = "Michael Rustler", start_year = NULL),
+  user = "mrustl"
+)
+
+kwb.pkgbuild::use_ghactions()
+
+kwb.pkgbuild::create_empty_branch_ghpages("tvl", org = "mrustl")
+
+usethis::use_pipe()
+usethis::use_vignette("workflow")
+desc::desc_normalize()
